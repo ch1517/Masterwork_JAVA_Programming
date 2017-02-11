e2b698ff846eecb0b14f67d4a6adaa24499bbdac
ch1517 <ch1517@naver.com>
2017-02-11 18:16:34 +0900

    Java Programming Example Chapter1

---
+++ Chapter1/Open_Challenge/Open_Challenge_1/src/Open_Challenge.java
@@ -0,0 +1,11 @@
+
+public class Open_Challenge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SuHyeon Jeong");
+		System.out.println("20 years old");
+		System.out.println("Department ofInformation & Communication Engineering");
+	}
+
+}
