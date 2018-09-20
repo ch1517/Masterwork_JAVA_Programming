4a79593f659cb82a5414a0c60eeb3a982c6d9c1c
JeongSuHyeon <25702775+ch1517@users.noreply.github.com>
2018-09-20 20:57:04 +0900

    Java Programming Example Chapter6

--- Chapter6/Chapter6/src/Query.java
+++ Chapter6/Chapter6/src/Query.java
@@ -1,6 +1,6 @@
 import java.util.StringTokenizer;
-
-public class Chapter6_10 {
+//Chapter6_10
+public class Query {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
