f0207a30e620a946a1281e392737d6b4b44b8fde
JeongSuHyeon <25702775+ch1517@users.noreply.github.com>
2018-09-20 19:12:56 +0900

    Java Programming Example Chapter6

--- Chapter6/Chapter6/src/Chapter6_6.java
+++ Chapter6/Chapter6/src/Chapter6_6.java
@@ -1,9 +1,25 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Chapter6_6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		InputStreamReader rd = new InputStreamReader(System.in);
+		int count=0;
+		try {
+			while(true) {
+				int c = rd.read(); // ctrl-z가 안먹혀서 Enter로 변경
+				if(c==13) // Enter 입력시 종료
+					break;
+				if((char)c>='A'&&(char)c<='Z') {
+					count++;
+				}
+			}
+			System.out.println(count);
+		} catch(IOException e) {
+			System.out.println("입력 오류 발생");
+		}
 	}
 
 }
